08e176decf778d223d26ad43d86ad9a098cda369
danieljlugo <daniellugofreelancer@gmail.com>
2023-01-20 09:31:08 -0300

    estructura inicial

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 
 import './App.css';
-// import Greeting from './pure/greeting';
-import GreetingF from './pure/greetingF';
+
 
 function App() {
   console.log("Los componentes deben ser reactivos")
   return (
     <div className="App">
-      <header className="App-header">
-         {/* <Greeting name="Daniel" /> */}
-         <GreetingF name="Martin"/>
-      </header>
+
     </div>
   );
 }
